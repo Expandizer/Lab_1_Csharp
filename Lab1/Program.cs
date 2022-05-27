de58022003720ef5aea2e4bf37418fa937c16835
Omar Boukmakh <main.expandizer@gmail.com>
2022-05-27 23:41:11 +0200

    lab 1 from exo 1 to 4.1

---
+++ Lab1/Program.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+
+//// Deadline : Vendredi 27 à 23h59
+//// Format du rendu : NOM_PRENOM_ST2API_TP1
+//// Multiplication tables
+
+//public static void Main(string[] args)
+//{
+//    for (int i = 1; i < 11; i++)
+//    {
+//        for (int j = 1; j < 11; j++)
+//        {
+//            Console.WriteLine(i * j);
+//        }
+//    }
+//    Console.ReadKey();
+//}
